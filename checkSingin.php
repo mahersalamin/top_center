ae0c2987b015fa83b60f202866444edebe79bf05
maher <salaminmaher@gmail.com>
2024-08-10 09:24:36 +0300

    prevent archived teacher from signing in

--- checkSingin.php
+++ checkSingin.php
@@ -4,46 +4,46 @@
 require 'dbconnection.php';
 
 
-
-
-
 if (isset($_POST['email']) && isset($_POST['password'])) {
 
-  $email = $_POST['email'];
-  $password =  $_POST['password'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
 
-  $Semail = mysqli_real_escape_string($conn, $email);
-  $Spassword = mysqli_real_escape_string($conn, $password);
+    $Semail = mysqli_real_escape_string($conn, $email);
+    $Spassword = mysqli_real_escape_string($conn, $password);
 
 
-  $query = "SELECT * FROM teacher WHERE user='$Semail' AND password='$Spassword'";
-  $result = mysqli_query($conn, $query);
+    $query = "SELECT * FROM teacher WHERE user='$Semail' AND password='$Spassword'";
+    $result = mysqli_query($conn, $query);
 
-
-  foreach ($result as $row) {
-
-    $Uid = $row['id'];
-    $name =  $row['name'];
-    $role =  $row['role'];
-  }
-
-  if (mysqli_num_rows($result) > 0) {
-
-    setcookie("id", $Uid, time() + 2000);
-    setcookie("name", $name, time() + 2000);
-    setcookie("role", $role, time() + 2000);
-
-    if ($role == 1) {
-
-      header("location:page/homeAdmin.php");
-    } else if ($role == 2) {
-      header("location:page/bodyHomeUser.php");
-    } else {
-      header("location:page/bodyHomeUser.php");
+    if($result['is_archived']==1){
+        header("location:page/signin.php?error=2");
     }
-  } else {
 
-    header("location:page/signin.php?error=1");
-  }
+    foreach ($result as $row) {
+
+        $Uid = $row['id'];
+        $name = $row['name'];
+        $role = $row['role'];
+    }
+
+    if (mysqli_num_rows($result) > 0) {
+
+        setcookie("id", $Uid, time() + 2000);
+        setcookie("name", $name, time() + 2000);
+        setcookie("role", $role, time() + 2000);
+
+        if ($role == 1) {
+
+            header("location:page/homeAdmin.php");
+        } else if ($role == 2) {
+            header("location:page/bodyHomeUser.php");
+        } else {
+            header("location:page/bodyHomeUser.php");
+        }
+    } else {
+
+        header("location:page/signin.php?error=1");
+    }
 }
