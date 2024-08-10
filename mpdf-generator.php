b181d378ce1e8974d3c0be63b5abc60cb0dc17cf
maher <salaminmaher@gmail.com>
2024-08-10 09:26:12 +0300

    updating reports

--- mpdf-generator.php
+++ mpdf-generator.php
@@ -6,7 +6,7 @@ $customerName = 'تقرير الدورات'; // Example customer name, you can d
 $companyPhone1 = "0569204082";
 $companyPhone2 = "02-2750628";
 $logoPath = './upload/top_logo.jpg';
-$companyAddress = 'بيت لحم/شارع القدس الخليل/ سوبر ماركت الراضي/ مجمع ابو سرور/مبنى الارتباط المدني/ الطابق الثاني';
+$companyAddress = 'بيت لحم / شارع القدس الخليل / مجمع ابو سرور ط2';
 use Mpdf\Mpdf;
 
 function generate_income_report($headers, $tableData): string
@@ -372,7 +372,7 @@ function generate_receipt($tableData): string {
         </head>
         <body>
             <div class='header'>
-            <img src='{$logoPath}' style='width: 100px; height: auto; display: block; margin: 0 auto;'>
+            <img src='{$logoPath}' style='width: 150px; height: auto; display: block; margin: 0 auto;'>
                 <h1>وصل دفع</h1>
                 <p><strong>اسم الشركة:</strong> {$companyName}</p>
                 <p><strong>عنوان الشركة:</strong> {$companyAddress}</p>
@@ -410,7 +410,7 @@ function generate_receipt($tableData): string {
                     <td>{$tableData['total_payments']}</td>
                 </tr>
                 <tr>
-                    <td>المبلغ المطلوب</td>
+                    <td>المبلغ الأصلي</td>
                     <td>{$tableData['session_cost']}</td>
                 </tr>
                 <tr>
@@ -704,228 +704,3 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-
-
-
-
-
-//require_once __DIR__ . '/vendor/autoload.php';
-//
-//use Mpdf\Mpdf;
-//
-//$companyName = 'مركز القمة التعليمي';
-//$companyPhone1 = "0569204082";
-//$companyPhone2 = "02-2750628";
-//$companyAddress = 'بيت لحم/شارع القدس الخليل/ سوبر ماركت الراضي/ مجمع ابو سرور/مبنى الارتباط المدني/ الطابق الثاني';
-//$logoPath = './upload/logo.jpg';
-//
-//function generateReportHeader(): string
-//{
-//    global $companyName, $companyAddress, $companyPhone1, $companyPhone2, $logoPath;
-//
-//    return '
-//    <div style="direction: rtl; text-align: center; padding: 20px;">
-//        <img src="' . $logoPath . '" style="width: 100px; height: auto; display: block; margin: 0 auto;">
-//        <div class="receipt-header">
-//            <div style="float: left; width: 50%;">
-//                <h5>' . $companyName . '</h5>
-//            </div>
-//            <div style="float: right; width: 50%; text-align: right;">
-//                <p style="direction: ltr">' . $companyPhone1 . ' <i class="fa fa-phone"></i></p>
-//                <p style="direction: ltr">' . $companyPhone2 . ' <i class="fa fa-phone"></i></p>
-//                <p>' . $companyAddress . ' <i class="fa fa-location-arrow"></i></p>
-//            </div>
-//            <div style="clear: both;"></div>
-//        </div>
-//    </div>';
-//}
-//
-//function generateTable($headers, $tableData): string
-//{
-//    $htmlTable = '<table border="1" style="width: 100%; border-collapse: collapse;">';
-//    $htmlTable .= '<thead><tr>';
-//    foreach ($headers as $header) {
-//        $htmlTable .= '<th>' . htmlspecialchars($header, ENT_QUOTES, 'UTF-8') . '</th>';
-//    }
-//    $htmlTable .= '</tr></thead><tbody>';
-//    foreach ($tableData as $row) {
-//        $htmlTable .= '<tr>';
-//        foreach ($row as $cell) {
-//            $htmlTable .= '<td>' . htmlspecialchars($cell, ENT_QUOTES, 'UTF-8') . '</td>';
-//        }
-//        $htmlTable .= '</tr>';
-//    }
-//    $htmlTable .= '</tbody></table>';
-//
-//    return $htmlTable;
-//}
-//
-//function generateReport($type, $headers, $tableData): string
-//{
-//    $headerHtml = generateReportHeader();
-//
-//    $htmlContent = $headerHtml . '<div>' . generateTable($headers, $tableData) . '</div>';
-//
-//    $date = date('Y-m-d');
-//    $issuer = 'السكرتيرة';
-//
-//    $htmlContent .= '
-//    <div class="receipt-footer" style="padding: 20px 0;">
-//        <div style="float: right; width: 20%;">
-//            <p><b>التاريخ: </b>' . $date . '</p>
-//        </div>
-//        <div style="float: left; width: 20%;">
-//            <p><b>' . $issuer . '</b></p>
-//        </div>
-//        <div style="clear: both;"></div>
-//    </div>';
-//
-//    return $htmlContent;
-//}
-//
-//function generateReceipt($tableData): string
-//{
-//    global $companyName, $logoPath, $companyAddress;
-//
-//    return "
-//    <html>
-//        <head>
-//            <style>
-//                body {
-//                    direction: rtl;
-//                    text-align: right;
-//                    font-family: 'Cairo', sans-serif;
-//                }
-//                table {
-//                    width: 100%;
-//                    border-collapse: collapse;
-//                }
-//                th, td {
-//                    border: 1px solid #ddd;
-//                    padding: 8px;
-//                }
-//                th {
-//                    background-color: #f2f2f2;
-//                }
-//                .header {
-//                    text-align: center;
-//                    margin-bottom: 20px;
-//                }
-//                .signature {
-//                    margin-top: 30px;
-//                    border-top: 1px solid #000;
-//                    text-align: right;
-//                    padding-top: 10px;
-//                }
-//            </style>
-//        </head>
-//        <body>
-//            <div class='header'>
-//                <img src='" . $logoPath . "' style='width: 100px; height: auto; display: block; margin: 0 auto;'>
-//                <h1>وصل دفع</h1>
-//                <p><strong>اسم الشركة:</strong> {$companyName}</p>
-//                <p><strong>عنوان الشركة:</strong> {$companyAddress}</p>
-//            </div>
-//            <table>
-//                <tr><td>التاريخ</td><td>{$tableData['date']}</td></tr>
-//                <tr><td>المستلم</td><td>{$tableData['cashier']}</td></tr>
-//                <tr><td>الدافع</td><td>{$tableData['payer']}</td></tr>
-//                <tr><td>الطالب</td><td>{$tableData['student_name']}</td></tr>
-//                <tr><td>الدورة</td><td>{$tableData['session_name']}</td></tr>
-//                <tr><td>المبلغ المدفوع</td><td>{$tableData['amount']}</td></tr>
-//                <tr><td>مجموع الدفعات</td><td>{$tableData['total_payments']}</td></tr>
-//                <tr><td>المبلغ المطلوب</td><td>{$tableData['session_cost']}</td></tr>
-//                <tr><td>حالة الدفع</td><td>{$tableData['payment_status']}</td></tr>
-//                <tr><td>ملاحظات</td><td>{$tableData['notes']}</td></tr>
-//            </table>
-//            <div class='signature'>
-//                <p>التوقيع: ...........................................</p>
-//                <p>السكرتيرة: ...........................................</p>
-//            </div>
-//        </body>
-//    </html>
-//    ";
-//}
-//
-//try {
-//    $type = $_POST['reportType'] ?? '';
-//    $headers = isset($_POST['headers']) ? json_decode($_POST['headers'], true) : [];
-//    $tableData = isset($_POST['tableData']) ? json_decode($_POST['tableData'], true) : [];
-//    $htmlContent = $_POST['htmlContent'] ?? '';
-//
-//    $mpdf = new Mpdf(['default_font' => 'Cairo']);
-//
-//    switch ($type) {
-//        case 'income_report':
-//            if (empty($headers) || empty($tableData)) {
-//                throw new Exception('No data received for income report.');
-//            }
-//            $reportHtml = generateReport($type, $headers, $tableData);
-//            $mpdf->WriteHTML($reportHtml);
-//            $mpdf->Output('income_report_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        case 'outcome_report':
-//            if (empty($headers) || empty($tableData)) {
-//                throw new Exception('No data received for outcome report.');
-//            }
-//            $reportHtml = generateReport($type, $headers, $tableData);
-//            $mpdf->WriteHTML($reportHtml);
-//            $mpdf->Output('outcome_report_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        case 'sessions_report':
-//            if (empty($headers) || empty($tableData)) {
-//                throw new Exception('No data received for sessions report.');
-//            }
-//            $reportHtml = generateReport($type, $headers, $tableData);
-//            $mpdf->WriteHTML($reportHtml);
-//            $mpdf->Output('sessions_report_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        case 'all_stats_report':
-//            if (empty($htmlContent)) {
-//                throw new Exception("No HTML content provided for all stats report.");
-//            }
-//            $htmlContent = generateReportHeader() . '<div>' . $htmlContent . '</div>';
-//            $mpdf->WriteHTML($htmlContent);
-//            $mpdf->Output('all_stats_report_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        case 'daily_report':
-//            if (empty($htmlContent)) {
-//                throw new Exception("No HTML content provided for all stats report.");
-//            }
-//            $htmlContent = generateReportHeader() . '<div>' . $htmlContent . '</div>';
-//            $mpdf->WriteHTML($htmlContent);
-//            $mpdf->Output('daily_report_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        case 'public_report':
-//            if (empty($htmlContent)) {
-//                throw new Exception("No HTML content provided for all stats report.");
-//            }
-//            $htmlContent = generateReportHeader() . '<div>' . $htmlContent . '</div>';
-//            $mpdf->WriteHTML($htmlContent);
-//            $mpdf->Output('public_report_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        case 'receipt':
-//            if (empty($tableData)) {
-//                throw new Exception("No table data provided for receipt.");
-//            }
-//            $receiptHtml = generateReceipt($tableData);
-//            $mpdf->WriteHTML($receiptHtml);
-//            $mpdf->Output('receipt_' . date('d-m-Y') . '.pdf', 'D');
-//            break;
-//
-//        default:
-//            throw new Exception('Invalid report type.');
-//    }
-//
-//} catch (Exception $e) {
-//    echo 'Error: ' . $e->getMessage();
-//}
-//?>
-
-
