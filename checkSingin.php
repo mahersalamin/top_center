4e1acd9d1fbba4ebec4b19ae205bf9f92a9edc9e
maher <salaminmaher@gmail.com>
2024-09-09 20:32:01 +0300

    prevent SQL injection

--- checkSingin.php
+++ checkSingin.php
@@ -5,46 +5,39 @@ require 'dbconnection.php';
 
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    // Prepare the statement
+    $stmt = $conn->prepare("SELECT * FROM teacher WHERE user = ? AND password = ?");
+    $stmt->bind_param("ss", $email, $password); // Bind parameters
 
-    $Semail = mysqli_real_escape_string($conn, $email);
-    $Spassword = mysqli_real_escape_string($conn, $password);
+    // Execute the statement
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-
-    $query = "SELECT * FROM teacher WHERE user='$Semail' AND password='$Spassword'";
-    $result = mysqli_query($conn, $query);
-
-
-
-    foreach ($result as $row) {
-
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
         $Uid = $row['id'];
         $name = $row['name'];
         $role = $row['role'];
         $is_archived = $row['is_archived'];
-    }
-    if($is_archived==1){
-        header("location:page/signin.php?error=2");
-    }
-    if (mysqli_num_rows($result) > 0) {
 
-        setcookie("id", $Uid, time() + 2000);
-        setcookie("name", $name, time() + 2000);
-        setcookie("role", $role, time() + 2000);
-
-        if ($role == 1) {
-
-            header("location:page/homeAdmin.php");
-        } else if ($role == 2) {
-            header("location:page/bodyHomeUser.php");
+        if ($is_archived == 1) {
+            header("location:page/signin.php?error=2");
         } else {
-            header("location:page/bodyHomeUser.php");
+            setcookie("id", $Uid, time() + 2000);
+            setcookie("name", $name, time() + 2000);
+            setcookie("role", $role, time() + 2000);
+
+            if ($role == 1) {
+                header("location:page/homeAdmin.php");
+            } else {
+                header("location:page/bodyHomeUser.php");
+            }
         }
     } else {
-
         header("location:page/signin.php?error=1");
     }
+
 }
